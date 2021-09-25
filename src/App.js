de3a0f9048e5c040b440d229c467ec00c7676c2f
Deespider <diptendu.biswas143@gmail.com>
2021-09-25 18:40:27 +0530

    Added API and made Working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,101 @@
-import React from 'react';
-// import styled from 'styled-components';
-//component
+import React, { Component } from 'react';
+import './App.css';
+import MovieRow from './MovieRow.js'
+import $ from 'jquery'
 
-import Header from './components/Header';
-import Home from './components/Home';
+class App extends Component {
 
-//Styles
-import {GlobalStyle} from './GlobalStyle';
+  constructor(props) {
+    super(props)
+    this.state = {}
+    // console.log("This is my initializer")
 
+    // const movies = [
+    //   {id: 0, poster_src: "https://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    //    title: "Avengers: Infinity War", overview: "As the Avengers and their allies have continued to protect the world from threats too large"},
+    //   {id: 1, poster_src: "https://image.tmdb.org/t/p/w185/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
+    //    title: "	The Avengers", overview: "This is my second overview"},
+    // ]
 
-// const Room= styled.div'
-//   position: relative;
-//   height: 500px;
-//   width: 500px;
-//   border: 10px solid-black;
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
 
-// ';
-function App() {
+    // this.state = {rows: movieRows}
 
-  return (
-    // (Room)
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-      <h1>Hey Use this</h1>
-    </div>
-  );
+    this.performSearch("ant man")
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        // console.log(searchResults)
+        const results = searchResults.results
+        // console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
+
+  render() {
+    return (
+      <div>
+        
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="50" src="green_app_icon.svg"/>
+              </td>
+              <td width="8"/>
+              <td>
+                <h1>MoviesDB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: "99%",
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term"/>
+
+        {this.state.rows}
+
+      </div>
+    );
+  }
 }
 
 export default App;
